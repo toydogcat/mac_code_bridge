5b7d9a215eb51de77a499b75f000c88d24453fe8
tobytoy <tobywang2021@gmail.com>
2024-02-17 10:59:40 +0800

    add gitignore

--- dummy_action/machine/run.py
+++ dummy_action/machine/run.py
@@ -10,12 +10,17 @@ from utils import tools
 # Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--state", type=str, default="run", help="run or ride.")
-parser.add_argument("-vp", "--video_path", type=str, default="datas/videos/MXBK1701_mxi01_30s.mp4", help="the video path.")
+parser.add_argument("-vp", "--video_path", type=str, default=None, help="the video path.")
+parser.add_argument("-vi", "--video_list_index", type=int, default=0, help="the video index in the video list.")
 args = parser.parse_args()
 
+video_path_list = [
+    "datas/videos/MXBK1701_mxi01_30s.mp4",
+    "/Users/owenchen/Movies/MXBK1701_mxi01.mp4"
+]
 
-
-video_path = args.video_path 
+video_path = args.video_path if args.video_path else video_path_list[args.video_list_index] 
+    
 image_path = "output.jpg"
 # keyboard_input = ""
 # mouse_input = ""
@@ -38,9 +43,13 @@ def mouse_event(event, x, y, flags, userdata):
     manager.modify_power(x, y)
     
     if event == 1 and flags == 1: # 左健
-        manager.developer = False
+        manager.modify_energy()
     if event == 2 and flags == 2: # 右健
-        manager.developer = True
+        manager.modify_state("ride") if manager.state == "run" else manager.modify_state("run")
+        if manager.developer:
+            manager.energy_water = 500
+            
+    
 cv2.setMouseCallback('live', mouse_event) 
 
 
@@ -60,10 +69,12 @@ while cap.isOpened():
     keycode = cv2.waitKey(1)
     if keycode == ord('q') or keycode == 27:
         break
+    if keycode == ord('z'):
+        manager.developer = False if manager.developer == True else True
     if keycode == 0:  # 按下鍵盤的「上」
-        manager.slope_modify = np.clip(manager.slope_modify + 0.02, 0, 0.2) 
+        manager.slope_modify_level = np.clip(manager.slope_modify_level + 1, 0, 10) 
     if keycode == 1:  # 按下鍵盤的「下」
-        manager.slope_modify = np.clip(manager.slope_modify - 0.02, 0, 0.2) 
+        manager.slope_modify_level = np.clip(manager.slope_modify_level - 1, 0, 10) 
     if keycode == 2:  # 按下鍵盤的「左」
         manager.resistance = np.clip(manager.resistance - 1, 1, 10) 
     if keycode == 3:  # 按下鍵盤的「右」
--- dummy_action/machine/utils/tools.py
+++ dummy_action/machine/utils/tools.py
@@ -2,60 +2,87 @@ import cv2
 import json
 import time
 import math
+import random
 import numpy as np
 
 
 class Manager:
     def __init__(self, video_path, state):
         # height, width, _ = img.shape
-        cap              = cv2.VideoCapture(video_path)
-        self.height      = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.width       = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.video_fps   = cap.get(cv2.CAP_PROP_FPS)  # the video average fps
-        self.frame_count = 0
+        cap                  = cv2.VideoCapture(video_path)
+        self.height          = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.width           = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.video_fps       = cap.get(cv2.CAP_PROP_FPS)  # the video average fps
+        self.frame_count_all = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame_count     = 0
         
         # warning
         self.warning_flag = False
         self.warning_num  = 1
         self.developer    = False
         
-        self.state = state
-        self.state_rate = 0.7 if state == "run" else 1.2
+        self.modify_state(state)
+        # self.state = state
+        # self.state_rate = 0.7 if state == "run" else 1.5
         
         self.normal_sleep_time = 1 / self.video_fps
 
         # Human info
         self.energy = 500.0
+        self.energy_water = 10
+        
         self.heart_rate = 60
-        self.resistance = 1
-        self.power_frame_output = 0
+        self.resistance = 5
+        
+        self.power_frame_output = 0  # 心願出力
+        self.power = 0               # 真實出力
+        
         self.energy_recover = 0
         self.energy_cost = 0
         self.slope_ground = 0
         self.theta = 0
-        self.slope_modify = 0 # machine can modify
+        
+        self.slope_modify_level = 5 # machine can modify
+        self.slope_modify = self.slope_modify_level * 0.02 
 
-        self.speed = 0
+        self.speed = -30
         self.acceleration = 0
         self.speed_rate = 1
         self.sleep_time = self.normal_sleep_time / self.speed_rate
         
         self.whiteboard = Whiteboard(100, self.width)
         
-    
+    def modify_state(self, state):
+        self.state = state
+        self.state_rate = 0.7 if state == "run" else 1.5
+        
+    def modify_energy(self):
+        self.energy = np.clip(self.energy + self.energy_water, 0, 500)
+        
     def modify_power(self, x, y):
-        self.power_frame_output = ( x + 100 + self.height - y ) / ( self.width + self.height )
+        self.power_frame_output = 5 * x / self.width
 
     def update_parameter(self):
         energy_left = 500 - self.energy
         if self.energy >= 100:
             self.heart_rate = energy_left / 16 + 60
+            random_rate = 2
         else:
             self.heart_rate = (energy_left - 400) * (3 / 20) + 85
+            random_rate = 10
 
-        self.energy_recover = math.log( self.heart_rate - 50 ) * 2 - 4
+        self.energy_recover = math.log( self.heart_rate - 50 ) * 2 - self.power - random_rate * random.random()
+        
+        self.power = self.power_frame_output * math.sqrt(self.energy/100) if self.energy < 100 else self.power_frame_output
+        self.energy_cost = self.power + (self.speed + 30) / 100
+        
+        self.energy += (self.energy_recover - self.energy_cost)
+        self.energy = np.clip(self.energy, 0, 500)
+        self.warning_flag = True if self.energy < 100 else False
+        
+        self.slope_modify = self.slope_modify_level * 0.02
         self.theta = math.atan(self.slope_ground + self.slope_modify)
-        self.acceleration = self.power_frame_output * self.state_rate * ( 1 - math.sin(self.theta) ) / ( self.resistance / 2 ) - 1
+        self.acceleration = (self.power * self.state_rate * ( 1 - math.sin(self.theta) ) / ( self.resistance / 2 ) - 1) / 5
 
         # slope ground need to get from json file
         # self.theta 
@@ -66,19 +93,17 @@ class Manager:
         else:
             self.speed_rate = (30 + self.speed) * (1 / 40) + (1 / 4) 
         self.sleep_time = self.normal_sleep_time / self.speed_rate
-        self.energy_cost = (self.speed + 30) / 10
-
-        self.energy += (self.energy_recover - self.energy_cost)
-        self.energy = np.clip(self.energy, 0, 500)
-        self.warning_flag = True if self.energy < 100 else False
+        
 
         # speed_rate
 
 
 
     def update(self, img):
-        self.frame = img
+        start_time = time.time()
         
+        self.frame_count += 1
+        self.frame = img
         self.update_parameter()
         
         # write info to whiteboard
@@ -87,12 +112,18 @@ class Manager:
         text = f"speed : {round(self.speed, 2)}"
         cv2.putText(self.whiteboard.img, 
                     text, (30, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
-        text = f"angle : {int(self.theta * 180 / math.pi)}"
+        text = f"power : {int(self.power * 20)}"
         cv2.putText(self.whiteboard.img, 
                     text, (180, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
-        text = f"resist : {self.resistance}"
+        text = f"angle : {int(self.theta * 180 / math.pi)}"
         cv2.putText(self.whiteboard.img, 
                     text, (330, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
+        text = f"resist : {self.resistance}"
+        cv2.putText(self.whiteboard.img, 
+                    text, (480, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
+        text = f"process : {int(100*self.frame_count/self.frame_count_all)} %"
+        cv2.putText(self.whiteboard.img, 
+                    text, (630, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
 
         text = f"state : {self.state}"
         cv2.putText(self.whiteboard.img, 
@@ -109,7 +140,7 @@ class Manager:
             cv2.putText(self.whiteboard.img, 
                         text, (900, 35), cv2.FONT_HERSHEY_COMPLEX, 0.5, (130, 130,0), 1, cv2.LINE_AA)
 
-            text = f"power : {round(self.power_frame_output, 4)}"
+            text = f"ipower : {round(self.power_frame_output, 4)}"
             cv2.putText(self.whiteboard.img, 
                         text, (1050, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (130, 130,0), 1, cv2.LINE_AA)
             text = f"energy : {round(self.energy, 4)}"
@@ -125,6 +156,13 @@ class Manager:
             cv2.putText(self.whiteboard.img, 
                         text, (1050, 95), cv2.FONT_HERSHEY_COMPLEX, 0.5, (130, 130,0), 1, cv2.LINE_AA)
             
+        end_time = time.time()
+        
+        if self.developer:
+            text = f"spend : {int((end_time - start_time)*1000)}"
+            cv2.putText(self.whiteboard.img, 
+                        text, (900, 55), cv2.FONT_HERSHEY_COMPLEX, 0.5, (130, 130,0), 1, cv2.LINE_AA)
+            
         
         # concat images
         self.image_out = cv2.vconcat([self.whiteboard.img, self.frame])
@@ -132,6 +170,7 @@ class Manager:
             h, w, _ = self.image_out.shape
             cv2.rectangle(self.image_out, (0,0), (w,h), (0,0,int(255/self.warning_num)), 10)
             self.warning_num = self.warning_num % 10 + 1  
+            
         
         
         
